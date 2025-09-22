c6b6495ab2f1f9bd03566ad52138314a423124c3
7lauanni <dias.lauanni@escola.pr.gov.br>
2025-09-22 07:53:06 -0300

    aula 22/09

--- script.js
+++ script.js
@@ -18,4 +18,38 @@ function processaTexto(texto){
 for (let i in palavras ){
     palavras [i] = palavras [i]. toLowerCastel();
 }
+
+palavras = tirapalavrasRuins(palavras);
+
+const frequencias = contaFrequencia(frequencia).short(ordenaPalavra);
+let ordenadas = Object.keys(frequencia) .short(ordenaPalavra);
+
+function ordenaPalavra(p1.p22){
+    return frequencias[p2] - frequencias[p1]
+}
+
+return ordenadas.slice(0,10);
+}
+
+function contaFrequencia(palavras){
+let frequencia = {};
+for (let i of palavras) {
+frequencias[1] =0;
+for (let j of palavras) {
+if (i == j){
+      frequencias
+         }
+      }
+    }
+return frequencias;
+}
+
+function tirapalavrasRuins(palavras){
+    const palavrasBoas = [];
+    for (let palavras of palavras){
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length >2){
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas
 }
